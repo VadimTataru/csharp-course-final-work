14a6ebe252537308459c5edd279fe6b93b785cc8
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-19 11:25:35 +0300

    feat: task 4 is done

---
+++ DotDetector/Circle.cs
@@ -0,0 +1,30 @@
+﻿namespace DotDetector
+{
+    internal class Circle
+    {
+        int x;
+        int y;
+        int radius;
+
+        public Circle(int x, int y, int radius)
+        {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        public Circle((int, int) point, int radius): this(point.Item1, point.Item2, radius)
+        {            
+        }
+
+        public bool IsPointBelong(int x, int y)
+        {
+            return (int)Math.Pow(this.x - x, 2) + (int)Math.Pow(this.y - y, 2) <= (int)Math.Pow(this.radius, 2);
+        }
+
+        public bool IsPointBelong((int, int) point)
+        {
+            return IsPointBelong(point.Item1, point.Item2);
+        }
+    }
+}
