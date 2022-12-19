f4f03048a7b0ba28250bf685eab8cab8ac25f8ae
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-19 10:00:04 +0300

    feat: done first task

---
+++ PrimeNumberApp/Program.cs
@@ -0,0 +1,35 @@
+﻿CheckNumber();
+
+void CheckNumber()
+{
+    while (true)
+    {
+        Console.WriteLine("Введите число для проверки: ");
+        int number;
+        if (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("Ввод не корректен");
+            continue;
+        }
+
+        if (IsNumberPrime(number))
+            Console.WriteLine($"Число {number} простое!");
+        else
+            Console.WriteLine($"Число {number} не является простым!");
+        return;
+    }
+    
+}
+
+
+bool IsNumberPrime(int number)
+{
+    if (number <= 1) return false;
+
+    int bound = (int)number / 2;
+    for(int i = 2; i < bound; i++)
+        if(number % i == 0)
+            return false;
+
+    return true;
+}
