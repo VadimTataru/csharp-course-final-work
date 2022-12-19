aa963feb00097df483d64fa4a16ed05445ba4029
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-19 11:29:20 +0300

    refactor: circleApp - replace int with double

--- DotDetector/Circle.cs
+++ DotDetector/Circle.cs
@@ -2,27 +2,27 @@
 {
     internal class Circle
     {
-        int x;
-        int y;
-        int radius;
+        double x;
+        double y;
+        double radius;
 
-        public Circle(int x, int y, int radius)
+        public Circle(double x, double y, double radius)
         {
             this.x = x;
             this.y = y;
             this.radius = radius;
         }
 
-        public Circle((int, int) point, int radius): this(point.Item1, point.Item2, radius)
+        public Circle((double, double) point, double radius): this(point.Item1, point.Item2, radius)
         {            
         }
 
-        public bool IsPointBelong(int x, int y)
+        public bool IsPointBelong(double x, double y)
         {
-            return (int)Math.Pow(this.x - x, 2) + (int)Math.Pow(this.y - y, 2) <= (int)Math.Pow(this.radius, 2);
+            return Math.Pow(this.x - x, 2) + Math.Pow(this.y - y, 2) <= Math.Pow(radius, 2);
         }
 
-        public bool IsPointBelong((int, int) point)
+        public bool IsPointBelong((double, double) point)
         {
             return IsPointBelong(point.Item1, point.Item2);
         }
--- DotDetector/Program.cs
+++ DotDetector/Program.cs
@@ -1,32 +1,38 @@
 ﻿using DotDetector;
 
 Circle circle = new Circle(0, -1, 2);
+Detect(circle);
 
-while (true)
+void Detect(Circle circle)
 {
-    Console.Clear();
-    Console.WriteLine("Введите координату [X]: ");
-    int x;
-    if (!int.TryParse(Console.ReadLine(), out x))
+    while (true)
     {
-        Console.WriteLine("Ввод не корректен");
-        continue;
-    }
-    Console.WriteLine("Введите координату [Y]: ");
-    int y;
-    if (!int.TryParse(Console.ReadLine(), out y))
-    {
-        Console.WriteLine("Ввод не корректен");
-        continue;
-    }
+        Console.Clear();
+        Console.WriteLine("Введите координату [X]: ");
+        double x;
+        if (!double.TryParse(Console.ReadLine(), out x))
+        {
+            Console.WriteLine("Ввод не корректен");
+            continue;
+        }
+        Console.WriteLine("Введите координату [Y]: ");
+        double y;
+        if (!double.TryParse(Console.ReadLine(), out y))
+        {
+            Console.WriteLine("Ввод не корректен");
+            continue;
+        }
 
-    if(circle.IsPointBelong(x, y))
-    {
-        Console.WriteLine($"Точка ({x}, {y}) находится внутри окружности или лежит на ней!");
-    } else
-    {
-        Console.WriteLine($"Точка ({x}, {y}) находится вне окружности!");
-    }
+        if (circle.IsPointBelong(x, y))
+        {
+            Console.WriteLine($"Точка ({x}, {y}) находится внутри окружности или лежит на ней!");
+        }
+        else
+        {
+            Console.WriteLine($"Точка ({x}, {y}) находится вне окружности!");
+        }
 
-    return;
+        return;
+    }
 }
+
