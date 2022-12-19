b85e0798f9954bbc6b1fba131f0ec8e472320599
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-19 12:00:23 +0300

    feat: task 5 is done

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,39 @@
+﻿DrawTable();
+
+void DrawTable()
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        for (int j = 0; j <= 10; j++)
+        {
+            if(i == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                if (j == 0)
+                    Console.Write($"     |");
+                else if(j < 10)
+                    Console.Write($"  {j}  |");
+                else
+                    Console.Write($"  {j} |");
+            } else if (j == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                if (i < 10)
+                    Console.Write($"  {i}  |");
+                else
+                    Console.Write($"  {i} |");
+            } else
+            {
+                Console.ResetColor();
+                int result = i * j;
+                if (result < 10)
+                    Console.Write($"  {result}  |");
+                else if (result < 100)
+                    Console.Write($"  {result} |");
+                else
+                    Console.Write($" {result} |");
+            }            
+        }
+        Console.WriteLine($"\n ________________________________________________________________ \n");
+    }
+}
