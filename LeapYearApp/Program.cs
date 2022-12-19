31c2c194645844b90df12f11da4d21d71bf83576
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-19 10:19:44 +0300

    feat: task 2 is done

---
+++ LeapYearApp/Program.cs
@@ -0,0 +1,31 @@
+﻿CheckYear();
+
+void CheckYear()
+{
+    while (true)
+    {
+        Console.WriteLine("Введите год в формате YYYY: ");
+        int year;
+        if (!int.TryParse(Console.ReadLine(), out year))
+        {
+            Console.WriteLine("Ввод не корректен");
+            continue;
+        }
+
+        if (IsLeap(year))
+            Console.WriteLine($"Год {year} високосный!");
+        else
+            Console.WriteLine($"Год {year} не является високосным!");
+        return;
+    }
+
+}
+
+
+bool IsLeap(int year)
+{
+    if(year % 4 == 0)
+        return true;
+    else
+        return false;
+}
