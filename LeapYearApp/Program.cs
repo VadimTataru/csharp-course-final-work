af9cda0e0d55c412c0232b4be5457e23de14b8b7
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-24 22:09:44 +0300

    fix: calculate leap year

--- LeapYearApp/Program.cs
+++ LeapYearApp/Program.cs
@@ -28,7 +28,7 @@ void CheckYear()
 /// </summary>
 bool IsLeap(int year)
 {
-    if(year % 4 == 0)
+    if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
         return true;
     else
         return false;
